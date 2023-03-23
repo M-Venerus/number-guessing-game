e087e9d99975004ab2c91cd844d067e1506916b7
M-Venerus <mikey.motorist@outlook.com>
2023-03-23 14:21:02 -0400

    Refactor and add docstrings

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,7 @@
 import random
 
-def attempts_start():
+def start_difficulty():
+    """Print beginning statement of guessing_game then get input for difficulty level."""
     print("The computer will choose a number between 1 and 100 at random. You have to guess it.")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard: ")
     if difficulty == "easy":
@@ -9,10 +10,13 @@ def attempts_start():
         return 5
 
 def randomNum():
+    """Generate random integer between 1 and 100."""
     num = random.randint(1, 100)
     return num
 
 def guess(attempts, hidden_number):
+    """Take a guess from user and compare it with a hidden number. User may guess again until
+    attempts run out. Return bool for guess if correct or if attemps run out."""
     while attempts > 0:
         print(f"You have {attempts} attempts remaining to guess the numer.")
         current_guess = int(input("Make a guess: "))
@@ -27,12 +31,13 @@ def guess(attempts, hidden_number):
     return False
     
 def conclusion(win_or_lose):
+    """Informs user if they won or lost."""
     if win_or_lose:
         print(f"You got it! You win! The number is {randomNum_selected}!")
     else:
         print(f"You are out of guesses, you lose. The number was {randomNum_selected}.")
 
 randomNum_selected = randomNum()
-starting_attempts = attempts_start()
+starting_attempts = start_difficulty()
 win_or_lose = guess(starting_attempts, randomNum_selected)
 conclusion(win_or_lose)
